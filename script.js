89130fdf448a5b9c2801b9d510c3a1d14b198e87
codeHasib <mohammadhasib9827@gmail.com>
2026-01-05 19:28:01 +0600

    Nav icon added for mobiles

--- script.js
+++ script.js
@@ -9,3 +9,21 @@ heroContactBtn.addEventListener("click", ()=> {
 closeBtn.addEventListener("click", ()=> {
     contactBox.style.display = "none";
 });
+
+const navBar = document.querySelector(".nav-bar");
+const navIcon = document.querySelector(".nav-icon");
+const navClose = document.querySelector(".nav-bar-close");
+
+let rotate = 0;
+
+navIcon.addEventListener("click", ()=> {
+    rotate += 360;
+    navIcon.style.transform = `rotate(${rotate}deg)`;
+    navBar.classList.add("active");
+});
+
+navClose.addEventListener("click", ()=> {
+    rotate += 360;
+    navClose.style.transform = `rotate(${rotate}deg)`;
+    navBar.classList.remove("active");
+});
